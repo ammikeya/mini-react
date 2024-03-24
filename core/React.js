e9ad3d4adedc49aaa44256fb32354693c76c5a5d
liushuiyuan <13783534092@sina.cn>
2024-03-24 18:06:06 +0800

    lesson3-4: fiber

--- core/React.js
+++ core/React.js
@@ -22,14 +22,83 @@ function createTextNode(text) {
 
 export function render(el, container) {
 
-	const dom = el.type === 'TEXT_ELEMENT' ? document.createTextNode("") : document.createElement(el.type)
-	for (const key in el.props) {
-		if(key !== 'children') {
-			dom[key] = el.props[key]
+	currWork = {
+		dom: container,
+		props: {
+			children: [el]
 		}
 	}
-	el.props.children.forEach(child => {
-		render(child, dom)
-	});
-	container.appendChild(dom)
+
+	// const dom = el.type === 'TEXT_ELEMENT' ? document.createTextNode("") : document.createElement(el.type)
+	// for (const key in el.props) {
+	// 	if(key !== 'children') {
+	// 		dom[key] = el.props[key]
+	// 	}
+	// }
+	// el.props.children.forEach(child => {
+	// 	render(child, dom)
+	// });
+	// container.appendChild(dom)
 }
+
+let currWork = null
+function work(idleDeadline) {
+
+	while(currWork && idleDeadline.timeRemaining()) {
+		// 渲染dom
+		const nextWork = performanceWorkDom(currWork)
+		currWork = nextWork
+	}
+	requestIdleCallback(work)
+}
+
+function createDom(type) {
+	return type === 'TEXT_ELEMENT' ? document.createTextNode("") : document.createElement(type)
+}
+
+function initProps(props, dom) {
+	for (const key in props) {
+		if(key !== 'children') {
+			dom[key] = props[key]
+		}
+	}
+}
+
+function updateChildren(fiber) {
+	let preChildFiber = null
+	fiber.props.children.forEach((child,index) => {
+		const childFiber = {
+			type: child.type,
+			props: child.props,
+			dom: null,
+			child: null,
+			sibling: null,
+			parent: fiber
+		}
+		if(index === 0) {
+			fiber.child = childFiber
+		} else {
+			preChildFiber.sibling = childFiber
+		}
+		preChildFiber = childFiber
+	});
+}
+
+function performanceWorkDom(fiber) {
+
+	if(!fiber.dom) {
+		const dom = (fiber.dom = createDom(fiber.type))
+		initProps(fiber.props, dom)
+		fiber.parent.dom.appendChild(dom)
+	}
+
+	updateChildren(fiber)
+
+	if(fiber.child) return fiber.child
+	if(fiber.sibling) return fiber.sibling
+
+	return fiber?.parent?.sibling
+}
+
+
+requestIdleCallback(work)
