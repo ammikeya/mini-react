28739926ba4396e80de400da7e5634cac7a02074
liushuiyuan <13783534092@sina.cn>
2024-04-05 11:01:32 +0800

    fix: 重构

--- core/React.js
+++ core/React.js
@@ -4,10 +4,14 @@ export function createElement(type, props, ...children) {
 		props: {
 			...props,
 			children: children.map(child => {
-				return typeof child === 'string' ? createTextNode(child) : child
+				return (typeof child === 'string' || typeof child === 'number') ? createTextNode(child) : child
 			})
 		}
 	}
+}
+
+export default {
+	createElement
 }
 
 function createTextNode(text) {
@@ -69,7 +73,14 @@ function commitRoot() {
 
 function commit(fiber) {
 	if(!fiber) return
-	fiber.parent.dom.appendChild(fiber.dom)
+
+	let fiberParent = fiber.parent
+	while(!fiberParent.dom) {
+		fiberParent = fiberParent.parent
+	}
+	if (fiber.dom) {
+    fiberParent.dom.appendChild(fiber.dom);
+  }
 	commit(fiber.child)
 	commit(fiber.sibling)
 }
@@ -86,9 +97,10 @@ function initProps(props, dom) {
 	}
 }
 
-function updateChildren(fiber) {
+function updateChildren(fiber, children) {
+
 	let preChildFiber = null
-	fiber.props.children.forEach((child,index) => {
+	children.forEach((child,index) => {
 		const childFiber = {
 			type: child.type,
 			props: child.props,
@@ -104,17 +116,29 @@ function updateChildren(fiber) {
 		}
 		preChildFiber = childFiber
 	});
+
+}
+
+function updateFunctionCompoent(fiber) {
+	updateChildren(fiber, [fiber.type(fiber.props)])
+}
+
+function updateHostComponent(fiber) {
+	if(!fiber.dom) {
+		const dom = (fiber.dom = createDom(fiber.type))
+		initProps(fiber.props, dom)
+	}
+	updateChildren(fiber, fiber.props.children)
 }
 
 function performanceWorkDom(fiber) {
 
-	if(!fiber.dom) {
-		const dom = (fiber.dom = createDom(fiber.type))
-		initProps(fiber.props, dom)
-		// fiber.parent.dom.appendChild(dom)
+	const isFunc = typeof fiber.type === 'function'
+	if(isFunc) {
+		updateFunctionCompoent(fiber)
+	} else {
+		updateHostComponent(fiber)
 	}
-
-	updateChildren(fiber)
 
 	if(fiber.child) return fiber.child
 	if(fiber.sibling) return fiber.sibling
